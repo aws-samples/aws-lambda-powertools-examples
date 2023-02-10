9e9dba651e72795f6285b27845917485f80af00e
Scott Gerring <gerrings@amazon.com>
2023-02-10 10:22:35 +0100

    test: ignore non-unit-test test

--- java/SqsBatchProcessing/Functions/src/test/java/org/demo/sqs/SqsPollerTest.java
+++ java/SqsBatchProcessing/Functions/src/test/java/org/demo/sqs/SqsPollerTest.java
@@ -1,10 +1,13 @@
 package org.demo.sqs;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SqsPollerTest {
 
+  // This test requires AWS access and cannot be run standalone like a unit test
   @Test
+  @Ignore
   public void successfulResponse() {
     SqsPoller app = new SqsPoller();
     app.handleRequest(null, null);
